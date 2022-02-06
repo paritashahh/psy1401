df114e8ac89e25396b235b4d784873a8efa2beea
PARITA SHAH <PARITASHAH@COLLEGE.HARVARD.EDU>
2022-02-06 13:23:30 -0500

    comments

--- proj1.py
+++ proj1.py
@@ -1,1 +1,7 @@
+#this project Implements Foldiak's learning rule and 
+#demonstrates how it learns a representation that is invariant to translation
+
+#have input vectors in an array
+#layer that classifies input vectors
+#output that has to be compared to the training data set w/ a loss function 
 
