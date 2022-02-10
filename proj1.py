1772aeee170fc186f8a933772f4d159e94b6001e
PARITA SHAH <PARITASHAH@COLLEGE.HARVARD.EDU>
2022-02-10 17:42:01 -0500

    training model

--- proj1.py
+++ proj1.py
@@ -1,242 +1,162 @@
-#this project Implements Foldiak's learning rule and 
-#demonstrates how it learns a representation that is invariant to translation
-
-#have input vectors in an array
-#layer that classifies input vectors
-#output that has to be compared to the training data set w/ a loss function 
-
-import pandas as pd
+# %% 
 import numpy as np
-import math 
-import random
-import matplotlib.pyplot as plt
-import string
-from scipy.spatial.transform import Rotation
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_iris
 
-'''
-# points a, b
-a, b = (0, 1, 0), (1, 0, 1)
+#diagonal sweeps
+r0 = [0,0,0,0,0,0,0,0]
+r1 = [1,0,0,0,0,0,0,0]
+r2 = [0,1,0,0,0,0,0,0]
+r3 = [0,0,1,0,0,0,0,0]
+r4 =[0,0,0,1,0,0,0,0]
+r5 =[0,0,0,0,1,0,0,0]
+r6 =[0,0,0,0,0,1,0,0]
+r7 =[0,0,0,0,0,0,1,0]
+r8 = [0,0,0,0,0,0,0,1]
 
-# matrix with row vectors of points
-A = np.array([a, b])
+#from middle up right
+diagonal_sweep1 =[]
 
-# 3x3 Identity transformation matrix
-I = np.eye(3)
+matrix = r1,r2,r3,r4,r5,r6,r7,r8
+matrix1 = np.asarray(matrix)
+for row in matrix:
+    matrix1 = np.delete(matrix1, 0, axis=0)
+    matrix1 = np.r_[matrix1, [r0]]
+    diagonal_sweep1.append(matrix1)
 
-# create the scaling transformation matrix
-T_s = np.array([[2, 0, 0], [0, 2, 0], [0, 0, 1]])
+#from up right down
+diagonal_sweep2 = diagonal_sweep1.copy()
+diagonal_sweep2 = np.flip(diagonal_sweep2, axis=0)
 
-# create the rotation transformation matrix
-T_r = np.array([[0, 1, 0], [-1, 0, 0], [0, 0, 1]])
+#from middle up left
+diagonal_sweep3 = diagonal_sweep1.copy()
+diagonal_sweep3 = np.flip(diagonal_sweep3, axis=1)
 
-color_lut = 'rgbc'
-fig = plt.figure()
-ax = plt.gca()
-xs = []
-ys = []
-for row in A:
-    output_row = I @ row
-    x, y, i = output_row
-    xs.append(x)
-    ys.append(y)
-    i = int(i) # convert float to int for indexing
-    c = color_lut[i]
-    plt.scatter(x, y, color=c)
-    plt.text(x + 0.15, y, f"{string.ascii_letters[i]}")
-xs.append(xs[0])
-ys.append(ys[0])
-plt.plot(xs, ys, color="gray", linestyle='dotted')
-ax.set_xticks(np.arange(-2.5, 3, 0.5))
-ax.set_yticks(np.arange(-2.5, 3, 0.5))
-plt.grid()
-plt.show()
+#from up left down
+diagonal_sweep4 = diagonal_sweep2.copy()
+diagonal_sweep4 = np.flip(diagonal_sweep4, axis=1)
 
-fig = plt.figure()
-ax = plt.gca()
-xs_s = []
-ys_s = []
-for row in A:
-    output_row = T_s @ row
-    x, y, i = row
-    x_s, y_s, i_s = output_row
-    xs_s.append(x_s)
-    ys_s.append(y_s)
-    i, i_s = int(i), int(i_s) # convert float to int for indexing
-    c, c_s = color_lut[i], color_lut[i_s] # these are the same but, its good to be explicit
-    plt.scatter(x, y, color=c)
-    plt.scatter(x_s, y_s, color=c_s)
-    plt.text(x + 0.15, y, f"{string.ascii_letters[int(i)]}")
-    plt.text(x_s + 0.15, y_s, f"{string.ascii_letters[int(i_s)]}'")
+#from middle down left
+diagonal_sweep5 = diagonal_sweep1.copy()
+diagonal_sweep5 = np.flip(diagonal_sweep5)
 
-xs_s.append(xs_s[0])
-ys_s.append(ys_s[0])
-plt.plot(xs, ys, color="gray", linestyle='dotted')
-plt.plot(xs_s, ys_s, color="gray", linestyle='dotted')
-ax.set_xticks(np.arange(-2.5, 3, 0.5))
-ax.set_yticks(np.arange(-2.5, 3, 0.5))
-plt.grid()
-plt.show()
+#from down left up
+diagonal_sweep6 = diagonal_sweep2.copy()
+diagonal_sweep6 = np.flip(diagonal_sweep6)
 
-fig = plt.figure()
-ax = plt.gca()
-for row in A:
-    output_row = T_r @ row
-    x_r, y_r, i_r = output_row
-    i_r = int(i_r) # convert float to int for indexing
-    c_r = color_lut[i_r] # these are the same but, its good to be explicit
-    letter_r = string.ascii_letters[i_r]
-    plt.scatter(x_r, y_r, color=c_r)
-    plt.text(x_r + 0.15, y_r, f"{letter_r}'")
+#from middle down right
+diagonal_sweep7 = diagonal_sweep3.copy()
+diagonal_sweep7 = np.flip(diagonal_sweep7)
 
-plt.plot(xs, ys, color="gray", linestyle='dotted')
-ax.set_xticks(np.arange(-2.5, 3, 0.5))
-ax.set_yticks(np.arange(-2.5, 3, 0.5))
-plt.grid()
-plt.show()
-'''
-
-#n is the number of dimensions 
-#returns a list containing the components of an unbiased unit vector
-def randomvector(n):
-    components = [np.random.normal() for i in range(n)]
-    r = math.sqrt(sum(x*x for x in components))
-    v = [x/r for x in components]
-    return v
-
-print(' '.join(map(str, randomvector(2)))) 
-
-#sample dataset using iris 
-iris_data = load_iris()
-df = pd.DataFrame(iris_data.data, columns=iris_data.feature_names)
-print(df)
-
-#splitting datset into training and testing data (80% to 20%)
-training_data, testing_data = train_test_split(df, test_size=0.2, random_state=25)
-
-print(f"No. of training examples: {training_data.shape[0]}")
-print(f"No. of testing examples: {testing_data.shape[0]}")
+#from down right up
+diagonal_sweep8 = diagonal_sweep4.copy()
+diagonal_sweep8 = np.flip(diagonal_sweep8)
 
 
-inputs = [[1, 1], [1, -1], [-1, 1], [-1,-1]]
+#vertical sweeps
+rLine = [1,1,1,1,1,1,1,1]
+rEmpty = [0,0,0,0,0,0,0,0]
 
-input_vectors = []
-def input(n_inputs) :
-    for i in range(n_inputs) :
-        input_vectors.append(randomvector(2))
+#down to up
+vertical_sweep1 = []
+matrix = rEmpty, rEmpty, rEmpty, rEmpty, rEmpty, rEmpty, rEmpty, rLine
+matrix2 = np.asarray(matrix)
+for row in matrix:
+    matrix2 = np.delete(matrix2, 0, axis=0)
+    matrix1 = np.r_[matrix2, [rEmpty]]
+    vertical_sweep1.append(matrix2)
 
-#layer function 
-'''
-class Layer:
-    def __init__(self, n_inputs, n_neurons):
-        self.weights = np.zeros((2, n_inputs))
-        self.biases = np.zeros((1, n_neurons))
-    def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
-        if np.all(self.output == 0):
-            self.biases += 1
+#up to down
+vertical_sweep2 = vertical_sweep1.copy()
+vertical_sweep2 = np.flip(vertical_sweep2, axis=0)
 
-'''
+#horisontal sweeps
+horisontal_sweep1 = []
+for matrix in vertical_sweep1:
+    matrix3 = matrix.T
+    horisontal_sweep1.append(matrix3)
 
-w1 = w2 = b = 0
-d_b = []
-d_w = []
-y = 1
-threshold = 1
+horisontal_sweep2 = []
+for matrix in vertical_sweep2:
+    matrix4 = matrix.T
+    horisontal_sweep1.append(matrix4)
 
-for i in (input_vectors) : 
-    d_b.append(b)
-    d_w.append([w1, w2])
-    w1 = w1 + (input_vectors[i][0] * y)
-    w2 = w2 + (input_vectors[i][1] * y)
-    b = b + y
+#______________________________________________________
 
-net_input = np.dot(input_vectors, d_w) + b
+#collapse matrix into array 
+#(now we can refer to each element aka simple unit individually)
+simple_units = np.asarray(matrix).flatten()
 
-def activation_function(net_input):
-    if net_input >= threshold:
-        return 1
-    elif -threshold <= net_input & net_input <= threshold:
-        return 0
-    else:
-        return -1
+#general constants!!
+#for actual implementation, 
+#let's add these as inputs to the computing function
+#so we can call the same function multiple times with different constants 
+#delta = 0.2
+#alpha = 0.02
 
-M1 = [
-    0, 1, 0, 0, 0, 0, 0, 0,
-    1, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 0, 0
-    ]
+#initial weights for each simple cell-unit pair sampled from uniform distribution
+##weights = #simple units
+w1j = np.random.uniform(0, 0.1, (len(r0)*len(matrix)))
+w2j = np.random.uniform(0, 0.1, (len(r0)*len(matrix)))
+w3j = np.random.uniform(0, 0.1, (len(r0)*len(matrix)))
+w4j = np.random.uniform(0, 0.1, (len(r0)*len(matrix)))
 
-M2 = [
-    0, 0, 1, 0, 0, 0, 0, 0,
-    0, 1, 0, 0, 0, 0, 0, 0,
-    1, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 0, 0
-    ]
+#initialize list to keep track of weight_change
+delta_w1j = []
+delta_w2j = []
+delta_w3j = []
+delta_w4j = []
 
+def complex_unit(wij): 
+    for i in range(len(simple_units)):
+        return (simple_units[i] * wij(i))
 
-M3 = [
-    0, 0, 0, 1, 0, 0, 0, 0,
-    0, 0, 1, 0, 0, 0, 0, 0,
-    0, 1, 0, 0, 0, 0, 0, 0,
-    1, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 0, 0
-    ]
+#compute which complex unit will be activated
+c1 = sum(complex_unit(w1j))
+c2 = sum(complex_unit(w2j))
+c3 = sum(complex_unit(w3j))
+c4 = sum(complex_unit(w4j))
 
-M4 = [
-    0, 0, 0, 0, 1, 0, 0, 0,
-    0, 0, 0, 1, 0, 0, 0, 0,
-    0, 0, 1, 0, 0, 0, 0, 0,
-    0, 1, 0, 0, 0, 0, 0, 0, 
-    1, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 0, 0, 
-    0, 0, 0, 0, 0, 0, 0, 0
-    ]
+#get index of max element (corresponds to which unit is activated)
+sum_list = [c1, c2, c3, c4]
+max_sum = max(sum_list)
+max_index = sum_list.index(max_sum)
 
-M5 =[
-    0, 0, 0, 0, 0, 1, 0, 0,
-    0, 0, 0, 0, 1, 0, 0, 0,
-    0, 0, 0, 1, 0, 0, 0, 0,
-    0, 0, 1, 0, 0, 0, 0, 0,
-    0, 1, 0, 0, 0, 0, 0, 0,
-    1, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0
-]
+#y is for every unit not every simple cell
+yt_prev = []
+yt = []
+y_avg = []
 
-random_int = random.sample(range(64), 64)
-c_unit1_sum = 0
-c_unit2_sum = 0
-c_unit3_sum = 0
-c_unit4_sum = 0
+#activate one of the complex units
+def unit_activation():
+    for i in range (4):
+        if i == max_index:
+            yt[i] = 1
+        else:
+            yt[i] = 0
 
-c_unit1 = random_int[0:16]
-c_unit2 = random_int[16:32]
-c_unit3 = random_int[32:48]
-c_unit4 = random_int[48:64]
+#compute y(t) 
+def y_bar(delta):
+    for i in range (4):
+        if yt_prev == []:
+            y_avg[i] = delta * yt
+        else:
+            y_avg[i] = (1 - delta)*(yt_prev[i]) + (delta)*(yt)
+    yt_prev = y_avg
 
-for i in (64):
-    while i < 16:
-        c_unit1_sum += M1[i] 
-    while 16 <= i < 32: 
-        c_unit2_sum += M1[i] 
-    while 32 <= i < 48:
-        c_unit3_sum += M1[i] 
-    while 48 <= i < 64: 
-         c_unit4_sum += M1[i] 
+#implement weight updating   
+def model(complex_unit, index, alpha):
+    weight_change = []
+    for xj, wij in zip(simple_units, complex_unit):
+        weight_change.append(alpha * y_avg(index) * (xj-wij))
+    for delta_wij, wij in zip(weight_change, complex_unit):
+        complex_unit.append(delta_wij + wij)
+
+def training():
+    unit_activation()
+    y_bar(0.2)
+    model(w1j, 0, 0.02)
+    model(w1j, 1, 0.02)
+    model(w1j, 2, 0.02)
+    model(w1j, 3, 0.02)
     
-activated_unit = max(c_unit1_sum, c_unit2_sum, c_unit3_sum, c_unit4_sum)
+# %%
+
